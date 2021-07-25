276a216a4915903e7716dd7618bc2190ae2b85df
Ariel Luna <ariel.leonardo.luna@gmail.com>
2021-07-25 20:35:54 -0300

    desafio #1

---
+++ js/miarchivo.js
@@ -0,0 +1,15 @@
+let nombre = prompt("Ingresa Tu Nombre");
+let password = prompt("Ingresa tu Contraseña");
+let elegir = Number(prompt("Por favor elija un número"));
+let saludo = prompt("Hola");
+
+if (elegir >= 1000) {
+    alert("superas los 1000");
+} else if (elegir >= 10 && elegir <= 50) {
+    alert("de 10 a 50")
+}
+if (saludo == "Hola") {
+    console.log("Bienvenido");
+} else {
+    alert("Saluda como corresponde");
+}
